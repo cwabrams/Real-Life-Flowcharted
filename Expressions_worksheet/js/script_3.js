5e0ea16eb1a5f05e17a38202139007f9c6c512ad
cwabrams <cwabrams@fullsail.edu>
2013-04-18 00:21:48 -0600

    Slice of Pie 2 tests

---
+++ Expressions_worksheet/js/script_3.js
@@ -0,0 +1,9 @@
+//Charles Abrams 4-17-2013 Expression Worksheet
+// JavaScript for Slice of Pie Part 2
+
+var numberSlices = 8; //Number of slices per Pizza
+var numberPeople = 4; //Total number of Partygoers
+var totalPizzas = 2; //Total pizzas ordered
+
+totalSlices = totalPizzas * numberSlices/numberPeople; //Total slices per person consumed
+console.log(totalSlices)

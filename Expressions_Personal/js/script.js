5a4ed1f29d417a5299a82032ba12c0659a11abc7
cwabrams <cwabrams@fullsail.edu>
2013-04-18 02:17:50 -0600

    Added 3rd prompt amount

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,4 +3,5 @@
 //Expenses report for my family
 
 var amountRent = prompt("Enter total rent for Month",950)
-var amountXcel = prompt("Enter total utility bill for Month")
+var amountXcel = prompt("Enter total for Xcel monthly bill")
+var amountCable = prompt("Enter total for Comcast monthly bill",75)

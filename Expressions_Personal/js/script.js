a798014ea0a62ff3da2875f69765f4846d034eec
cwabrams <cwabrams@fullsail.edu>
2013-04-18 03:40:37 -0600

    Successful change to parseInt

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,7 +4,7 @@
 
 var amountRent = parseInt(prompt("Enter total rent for Month",950));//Amount for rent
 var amountExcel = parseInt(prompt("Enter total for Excel monthly bill")); //Amount for Excel
-var amountCable = parseInt(prompt("Enter total for Comcast monthly bill,75")); //Amount for Cable
+var amountCable = parseInt(prompt("Enter total for Comcast monthly bill",75)); //Amount for Cable
 var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill",300)); // Amount for Groceries
 //var amountPaycheck = prompt("Enter paycheck amount")
 

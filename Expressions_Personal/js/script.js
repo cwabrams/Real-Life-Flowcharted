f64f66956d2b140bdd1cac7928a60a7c3d406397
cwabrams <cwabrams@fullsail.edu>
2013-04-18 07:22:37 -0600

    Changed order of prompt and fixed typo

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,9 +6,10 @@ var amountRent = parseInt(prompt("Enter total rent for Month",950));//Amount for
 var amountExcel = parseInt(prompt("Enter total for Excel monthly bill")); //Amount for Excel
 var amountCable = parseInt(prompt("Enter total for Comcast monthly bill",75)); //Amount for Cable
 var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill",300)); // Amount for Groceries
-var amountPaycheck = parseInt(prompt("Enter paycheck amount")); //Amount for paycheck
 var miscExpenses = parseInt(promt("Enter Miscellaneous expense amount")); //Added misc. expense amount 
-var miscName = promtp("Enter reason for Miscellaneous expense"); //Bringing Misc reason as string
+var miscName = prompt("Enter reason for Miscellaneous expense"); //Bringing Misc reason as string
+var amountPaycheck = parseInt(prompt("Enter paycheck amount")); //Amount for paycheck
+
 
 var totalBills = amountRent + amountExcel + amountCable + amountGrocery; //Bill total calculation
 var amountLeft = amountPaycheck - totalBills; //Savings left over

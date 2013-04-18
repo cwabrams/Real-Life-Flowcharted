9d80f74e70197963149a0cd73654a341be0ddcc3
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:42:01 -0600

    Typo fix for prompt

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -3,7 +3,7 @@
 //Calculating the total time I spent on parsers for a given week
 
 var parserEdit = prompt ("Enter total number of parser edits",20) //Total edits entry
-var parserCreate = promt ("Enter how many parsers you created",5) //Total creations entry
+var parserCreate = prompt ("Enter how many parsers you created",5) //Total creations entry
 var parserCreatetime = prompt ("Enter average time spent per parser in hours", 1) //Hour average per parser creation
 
 totalTime = parserEdit + parserCreate * parserCreatetime

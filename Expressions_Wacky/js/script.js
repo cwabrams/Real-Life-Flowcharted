85f87be595f2100bbf1d85d0f583443ae4c9ea8a
cwabrams <cwabrams@fullsail.edu>
2013-04-18 08:13:46 -0600

    Notes left for later

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,6 +1,7 @@
 // Charles Abrams 4-17-13 Expressions Assignment (Wacky)
 // JavaScript
 // Creating Array to be used for which values you hold higher than others
+// Need to find out how to link selected house to keyword
 
 var hogwartHouses = ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"];
 hogwartHouses [0] = "Brave";

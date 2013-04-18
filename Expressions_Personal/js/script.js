7fbc8b495bfd7bdfe4158bc9ff0997f34a604447
cwabrams <cwabrams@fullsail.edu>
2013-04-18 07:29:46 -0600

    Default value removal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,11 +1,12 @@
 // Charles Abrams 4-17-13 Expressions_Personal
 //Javascript
-//Monthly Required Expense report for my family
+//Monthly Required Expense report for my family:  This is meant to be used by anyone for basic bills, but has specific fields added for my girlfriend's use.
+//Default values removed for purposes of assignment
 
-var amountRent = parseInt(prompt("Enter total rent for Month",950));//Amount for rent
+var amountRent = parseInt(prompt("Enter total rent for Month"));//Amount for rent
 var amountExcel = parseInt(prompt("Enter total for Excel monthly bill")); //Amount for Excel
-var amountCable = parseInt(prompt("Enter total for Comcast monthly bill",75)); //Amount for Cable
-var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill",300)); // Amount for Groceries
+var amountCable = parseInt(prompt("Enter total for Comcast monthly bill")); //Amount for Cable
+var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill")); // Amount for Groceries
 var miscExpense = parseInt(prompt("Enter Miscellaneous expense amount")); //Added misc. expense amount 
 var miscName = prompt("Enter reason for Miscellaneous expense"); //Bringing Misc reason as string
 var amountPaycheck = parseInt(prompt("Enter paycheck amount")); //Amount for paycheck

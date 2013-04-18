27e24c77e4cdc52ffe7b8324ea9ea625c6ec3a2f
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:43:40 -0600

    Edited this for my entire team vs. myself

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -1,9 +1,9 @@
 // Charles Abrams 4-17-13 Expressions_Industry
 
-//Calculating the total time I spent on parsers for a given week
+//Calculating the total time our team spent on parsers for a given week
 
 var parserEdit = prompt ("Enter total number of parser edits",10) //Total edits entry
-var parserCreate = prompt ("Enter how many parsers you created",2) //Total creations entry
+var parserCreate = prompt ("Enter how many parsers we created",2) //Total creations entry
 var parserCreatetime = prompt ("Enter average time spent per parser in hours", 1) //Hour average per parser creation
 
 totalTime = parserEdit + parserCreate * parserCreatetime

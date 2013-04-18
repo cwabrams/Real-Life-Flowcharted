8fc66849767a4577044a70ddc1876e33cff861b1
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:51:11 -0600

    Adding additional comments
    Deciding if I want default values to appear vs. leaving the prompts
    blank.

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -1,6 +1,6 @@
 // Charles Abrams 4-17-13 Expressions_Industry
 
-//Calculating the total time our team spent on parsers for a given week
+//Calculating the total time our team spent on parsers for a given week  This is used by our finance team for billing purposes to our clients
 
 var parserEdit = prompt ("Enter total number of parser edits",10) //Total edits entry
 var parserCreate = prompt ("Enter how many parsers we created",2) //Total creations entry

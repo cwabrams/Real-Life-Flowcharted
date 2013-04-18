14a3e3ee263bb287469b84389cc132dac7639749
cwabrams <cwabrams@fullsail.edu>
2013-04-17 23:09:13 -0600

    Arrays in Expressions Practice

--- What's under the hood/js/script.js
+++ What's under the hood/js/script.js
@@ -1,9 +1,6 @@
 // JavaScript
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+var orangeBins = [87, 234, 567, 883];
 
-var average= (quiz1 + quiz2 + quiz3 + quiz4)/4;
-console.log(average); 
+var total= orangeBins[0] + orangeBins[1] + orangeBins[2] + orangeBins[3];
+console.log(total); 

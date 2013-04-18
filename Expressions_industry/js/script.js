ed48076c8320abfe8f95f6226b4b3fa327b25826
cwabrams <cwabrams@fullsail.edu>
2013-04-18 02:06:09 -0600

    Formatting change (Final edit)
    Final edit

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -1,7 +1,6 @@
 // Charles Abrams 4-17-13 Expressions_Industry
 
 //Calculating the total time our team spent on parsers for a given week  This is used by our finance team for billing purposes to our clients
-//
 
 var parserEdit = prompt ("Enter total number of parser edits") //Total edits entry
 var parserCreate = prompt ("Enter how many parsers we created") //Total creations entry

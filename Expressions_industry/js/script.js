140b9fcca12cc7df4bf7e2f1da323358611d1c1c
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:57:16 -0600

    Adding last var, subtraction
    This client is not billed for these services

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -1,10 +1,12 @@
 // Charles Abrams 4-17-13 Expressions_Industry
 
 //Calculating the total time our team spent on parsers for a given week  This is used by our finance team for billing purposes to our clients
+//
 
 var parserEdit = prompt ("Enter total number of parser edits") //Total edits entry
 var parserCreate = prompt ("Enter how many parsers we created") //Total creations entry
 var parserCreatetime = prompt ("Enter average time spent per parser in hours") //Hour average per parser creation
+var customer1TIME = prompt ("Enter time spent on Customer A parsers in hours") //Finance does not bill for this client, so these are removed
 
 totalTime = parserEdit + parserCreate * parserCreatetime
 

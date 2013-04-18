8b301521e3810b8326986684198def77c25111a6
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:38:07 -0600

    New var

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -5,3 +5,5 @@
 var parserEdit = prompt ("Enter total number of parser edits",20) //Total edits entry
 var parserEdittime = .5 //Half an hour average per parser
 var parserCreate = promt ("Enter how many parsers you created",5) //Total creations entry
+var parserCreatetime = 1 //Hour average per parser creation
+

2a21a5d94f0eef3b1c43d254ca16c291278d9801
cwabrams <cwabrams@fullsail.edu>
2013-04-18 00:04:54 -0600

    Test Complete
    Formula working, added comments for each line

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,7 +1,7 @@
 //Charles Abrams 4-17-2013 Expression Worksheet
 // JavaScript for Dog Years
 
-var sparkyAge = 2; 
-var dogYears = sparkyAge * 7;
-console.log(dogYears);
+var sparkyAge = 2; //Whatever age Sparky is in actual years
+var dogYears = sparkyAge * 7; //Formula for calculating dog years based on given entry
+console.log(dogYears); //Print to console
 

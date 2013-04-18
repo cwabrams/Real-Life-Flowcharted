80de048bb218eee85367d3f6c249423033011876
cwabrams <cwabrams@fullsail.edu>
2013-04-18 08:08:25 -0600

    Finishing with equals signs

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,13 +2,14 @@
 // JavaScript
 // Creating Array to be used for which Hogwarts house you should be sorted into
 
-var hogwartHouses ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"];
-var selectedHouse1 prompt("Please enter your 1st Favorite Hogwarts house"); //User choice of house
-var selectedHouse2 prompt("Please enter your 2nd Favorite Hogwarts house"); //Second user choice of house
+var hogwartHouses = ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"];
 hogwartHouses [0] = "Brave";
 hogwatrHouses [1] = "Intelligent";
 hogwartHouses [2] = "Loyal";
 hogwatrHouses [3] = "Powerful";
+var selectedHouse1 = prompt("Please enter your 1st Favorite Hogwarts house"); //User choice of house
+var selectedHouse2 = prompt("Please enter your 2nd Favorite Hogwarts house"); //Second user choice of house
+
 
 
 console.log(hogwartHouses [0]);

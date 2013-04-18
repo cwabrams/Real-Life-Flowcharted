d90b3e8ec95ee1864db45f6796640a4c472efad5
cwabrams <cwabrams@fullsail.edu>
2013-04-18 03:56:41 -0600

    Printout changes
    Not getting the prinout to work as I would like.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,16 +2,16 @@
 //Javascript
 //Monthly Required Expense report for my family
 
-var amountRent = 	(prompt("Enter total rent for Month",950));//Amount for rent
+var amountRent = parseInt(prompt("Enter total rent for Month",950));//Amount for rent
 var amountExcel = parseInt(prompt("Enter total for Excel monthly bill")); //Amount for Excel
 var amountCable = parseInt(prompt("Enter total for Comcast monthly bill",75)); //Amount for Cable
 var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill",300)); // Amount for Groceries
 var amountPaycheck = parseInt(prompt("Enter paycheck amount")); //Amount for paycheck
 
 var totalBills = amountRent + amountExcel + amountCable + amountGrocery; //Bill total calculation
-var amountLeft = amountPaycheck - totalBills //Savings left over
+var amountLeft = amountPaycheck - totalBills; //Savings left over
 
+console.log("Our total bills were" + " " totalBills)
 console.log(totalBills)
 console.log(amountLeft)
 
-console.log("Our total bills were" + " $" + totalBills + " " + "and we have" + " $" amountLeft + "for other expenses"); //Printout 

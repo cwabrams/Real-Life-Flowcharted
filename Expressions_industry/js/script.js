5b1213326f378d1e95881adbd86debdc6a3b77e0
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:51:54 -0600

    Test without default value

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -2,7 +2,7 @@
 
 //Calculating the total time our team spent on parsers for a given week  This is used by our finance team for billing purposes to our clients
 
-var parserEdit = prompt ("Enter total number of parser edits",10) //Total edits entry
+var parserEdit = prompt ("Enter total number of parser edits") //Total edits entry
 var parserCreate = prompt ("Enter how many parsers we created",2) //Total creations entry
 var parserCreatetime = prompt ("Enter average time spent per parser in hours", 1) //Hour average per parser creation
 

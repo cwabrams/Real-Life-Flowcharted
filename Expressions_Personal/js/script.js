83941794c022c600103596f4fcb8a1c553121260
cwabrams <cwabrams@fullsail.edu>
2013-04-18 03:15:05 -0600

    parseint

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,6 +3,7 @@
 //Monthly Required Expense report for my family
 
 var amountRent = prompt("Enter total rent for Month");//Amount for rent
+parseInt amountRent
 var amountExcel = prompt("Enter total for Excel monthly bill"); //Amount for Excel
 var amountCable = prompt("Enter total for Comcast monthly bill"); //Amount for Cable
 var amountGrocery = prompt("Enter estimated Grocery monthly bill"); // Amount for Groceries

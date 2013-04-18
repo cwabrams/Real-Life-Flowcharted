9fe7eccf1f15f7809baf0f8722dfa824649e40b6
cwabrams <cwabrams@fullsail.edu>
2013-04-18 02:05:08 -0600

    Testing alert
    Want to test to see if alert will pop

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -8,7 +8,9 @@ var parserCreate = prompt ("Enter how many parsers we created") //Total creation
 var parserCreatetime = prompt ("Enter average time spent per parser in hours") //Hour average per parser creation
 var customer1Time = prompt ("Enter time spent on Customer A parsers in hours") //Finance does not bill for this client, so these are removed
 
-totalTime = parserEdit + parserCreate * parserCreatetime - customer1Time
+totalTime = parserEdit + parserCreate * parserCreatetime - customer1Time // Calculation for Total time
 
-console.log("Total we spent" + " " + totalTime + " " + "team hours this week on parsers")
+console.log("Total we spent" + " " + totalTime + " " + "team hours this week on parsers") //Printout for total time
+
+alert("Total we spent" + " " + totalTime + " " + "team hours this week on parsers")
 

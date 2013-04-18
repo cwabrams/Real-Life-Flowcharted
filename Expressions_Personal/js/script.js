ed4afa9f3fd322d24094df114718bdf2138fcb17
cwabrams <cwabrams@fullsail.edu>
2013-04-18 07:26:15 -0600

    Added misc exp
    To total bill calc

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,16 +6,16 @@ var amountRent = parseInt(prompt("Enter total rent for Month",950));//Amount for
 var amountExcel = parseInt(prompt("Enter total for Excel monthly bill")); //Amount for Excel
 var amountCable = parseInt(prompt("Enter total for Comcast monthly bill",75)); //Amount for Cable
 var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill",300)); // Amount for Groceries
-var miscExpenses = parseInt(prompt("Enter Miscellaneous expense amount")); //Added misc. expense amount 
+var miscExpense = parseInt(prompt("Enter Miscellaneous expense amount")); //Added misc. expense amount 
 var miscName = prompt("Enter reason for Miscellaneous expense"); //Bringing Misc reason as string
 var amountPaycheck = parseInt(prompt("Enter paycheck amount")); //Amount for paycheck
 
 
-var totalBills = amountRent + amountExcel + amountCable + amountGrocery; //Bill total calculation
+var totalBills = amountRent + amountExcel + amountCable + amountGrocery + miscExpese; //Bill total calculation
 var amountLeft = amountPaycheck - totalBills; //Savings left over
 
 console.log("Our total bills were" + " $" + totalBills + " " + "and our potential savings equal" + " $" + amountLeft) 
-console.log(miscName + " " + "costing" + " " + miscExpenses)//Printout
+console.log(miscName + " " + "costing" + " " + miscExpense)//Printout
 
 
 

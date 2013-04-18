487c3c9abc65b0041a80e053bc34279dcc0e320a
cwabrams <cwabrams@fullsail.edu>
2013-04-18 02:21:39 -0600

    Commit for new vars and calc

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,11 @@
 // Charles Abrams 4-17-13 Expressions Assignment (Personal)
 //Javascript
-//Expenses report for my family
+//Monthly Required Expense report for my family
 
 var amountRent = prompt("Enter total rent for Month",950)
 var amountXcel = prompt("Enter total for Xcel monthly bill")
 var amountCable = prompt("Enter total for Comcast monthly bill",75)
+var amountGrocery = prompt("Enter estimated Grocery monthly bill",300)
+var amountPaycheck = prompt(Enter bi-monthly
+
+totalBills = amountRent + amountXcel + amountCable + amountGrocery

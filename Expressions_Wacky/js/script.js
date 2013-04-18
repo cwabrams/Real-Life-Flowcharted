3dff2dddbafb5a9aa86fa07deba7a4a32f59284c
cwabrams <cwabrams@fullsail.edu>
2013-04-18 07:54:36 -0600

    Prompts added
    for House selection

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,4 +2,7 @@
 // JavaScript
 // Creating Array to be used for which Hogwarts house you should be sorted into
 
-var hogwartHouses [Gryffindor, Ravenclaw, Hufflepuff, Slytherin]
+var hogwartHouses [Gryffindor, Ravenclaw, Hufflepuff, Slytherin];
+var selectedHouse1 prompt("Please enter your 1st Favorite Hogwarts house"); //User choice of house
+var selectedHouse2 prompt("Please enter your 2nd Favorite Hogwarts house"); //Second user choice of house
+var houseGryffindor = 

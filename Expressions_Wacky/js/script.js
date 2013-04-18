c3e197df43110a1ebd82ae29125a214665c831ed
cwabrams <cwabrams@fullsail.edu>
2013-04-18 17:50:49 -0600

    Last phrase change

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -11,7 +11,7 @@ console.log("You selected" + " " + selectedHouse); //Print to console choice use
 hogwartHouses [0] = "brave at heart";
 hogwartHouses [1] = "of wit and learning";
 hogwartHouses [2] = "just and loyal friends";
-hogwartHouses [3] = "power hungry";
+hogwartHouses [3] = "cunning and power hungry";
 
 switch (selectedHouse) 
 {

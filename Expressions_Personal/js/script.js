ebc6ecece1c433e3d7847ad6c784fa828c073052
cwabrams <cwabrams@fullsail.edu>
2013-04-18 02:46:43 -0600

    Added semicolons...

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,15 +2,13 @@
 //Javascript
 //Monthly Required Expense report for my family
 
-var amountRent = prompt("Enter total rent for Month") //Amount for rent
-var amountXcel = prompt("Enter total for Xcel monthly bill") //Amount for Xcel
-var amountCable = prompt("Enter total for Comcast monthly bill") //Amount for Cable
-var amountGrocery = prompt("Enter estimated Grocery monthly bill") // Amount for Groceries
+var amountRent = prompt("Enter total rent for Month");//Amount for rent
+var amountXcel = prompt("Enter total for Xcel monthly bill"); //Amount for Xcel
+var amountCable = prompt("Enter total for Comcast monthly bill"); //Amount for Cable
+var amountGrocery = prompt("Enter estimated Grocery monthly bill"); // Amount for Groceries
 //var amountPaycheck = prompt("Enter paycheck amount")
 
-totalBills = amountRent + amountXcel 
-
-//+ amountCable + amountGrocery
+totalBills = amountRent + amountXcel + amountCable + amountGrocery;
 //amountLeft = amountPaycheck - totalBills
 
 console.log(totalBills)

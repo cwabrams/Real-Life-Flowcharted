c1be001406086db40aac350cd50d36eb7c71a106
cwabrams <cwabrams@fullsail.edu>
2013-04-18 03:39:46 -0600

    Fix for parseInt

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,14 +2,10 @@
 //Javascript
 //Monthly Required Expense report for my family
 
-var amountRent = prompt("Enter total rent for Month");//Amount for rent
-parseInt = (amountRent)
-var amountExcel = prompt("Enter total for Excel monthly bill"); //Amount for Excel
-parseInt = (amountExcel)
-var amountCable = prompt("Enter total for Comcast monthly bill"); //Amount for Cable
-parseInt = (amountCable)
-var amountGrocery = prompt("Enter estimated Grocery monthly bill"); // Amount for Groceries
-parseInt = (amountGrocery)
+var amountRent = parseInt(prompt("Enter total rent for Month",950));//Amount for rent
+var amountExcel = parseInt(prompt("Enter total for Excel monthly bill")); //Amount for Excel
+var amountCable = parseInt(prompt("Enter total for Comcast monthly bill,75")); //Amount for Cable
+var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill",300)); // Amount for Groceries
 //var amountPaycheck = prompt("Enter paycheck amount")
 
 var totalBills = amountRent + amountExcel + amountCable + amountGrocery;

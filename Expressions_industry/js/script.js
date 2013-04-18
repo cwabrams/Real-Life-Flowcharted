b121d6e9bce36dc167a41a4e127c1ac0102eaea8
cwabrams <cwabrams@fullsail.edu>
2013-04-18 03:43:36 -0600

    Parseint change

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -2,12 +2,12 @@
 
 //Calculating the total time our team spent on parsers for a given week  This is used by our finance team for billing purposes to our clients
 
-var parserEdit = prompt ("Enter total number of parser edits"); //Total edits entry
-var parserCreate = prompt ("Enter how many parsers we created"); //Total creations entry
-var parserCreatetime = prompt ("Enter average time spent per parser in hours"); //Hour average per parser creation
-var customer1Time = prompt ("Enter time spent on Customer A parsers in hours"); //Finance does not bill for this client, so these are removed
+var parserEdit = parseInt(prompt("Enter total number of parser edits")); //Total edits entry
+var parserCreate = parseInt(prompt("Enter how many parsers we created")); //Total creations entry
+var parserCreatetime = parseInt(prompt("Enter average time spent per parser in hours")); //Hour average per parser creation
+var customer1Time = parseInt(prompt("Enter time spent on Customer A parsers in hours")); //Finance does not bill for this client, so these are removed
 
-totalTime = parserEdit + parserCreate * parserCreatetime - customer1Time; // Calculation for Total time
+var totalTime = parserEdit + parserCreate * parserCreatetime - customer1Time; // Calculation for Total time
 
 console.log("Total we spent" + " " + totalTime + " " + "team hours this week on parsers"); //Printout for total time
 

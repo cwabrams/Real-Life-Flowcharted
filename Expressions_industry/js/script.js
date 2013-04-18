20457f28e18cb250b5ea407238bd523165692d51
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:42:56 -0600

    Added new default values

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -2,8 +2,8 @@
 
 //Calculating the total time I spent on parsers for a given week
 
-var parserEdit = prompt ("Enter total number of parser edits",20) //Total edits entry
-var parserCreate = prompt ("Enter how many parsers you created",5) //Total creations entry
+var parserEdit = prompt ("Enter total number of parser edits",10) //Total edits entry
+var parserCreate = prompt ("Enter how many parsers you created",2) //Total creations entry
 var parserCreatetime = prompt ("Enter average time spent per parser in hours", 1) //Hour average per parser creation
 
 totalTime = parserEdit + parserCreate * parserCreatetime

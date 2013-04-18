03b82b64dac537d890f6e2c0ebb0f59e0ed86fba
cwabrams <cwabrams@fullsail.edu>
2013-04-18 07:36:54 -0600

    Wacky JS file first commit

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,5 +1,4 @@
+// Charles Abrams 4-17-13 Expressions Assignment (Wacky)
 // JavaScript
+// Creating Array to be used for which Hogwarts house you should be sorted into
 
-var a = 3;
-a --; 
-console.log(a);

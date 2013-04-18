a75d62f5f557353c0186f07201393655bbb3f76b
cwabrams <cwabrams@fullsail.edu>
2013-04-18 03:34:12 -0600

    Still no luck with Int

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,13 +3,13 @@
 //Monthly Required Expense report for my family
 
 var amountRent = prompt("Enter total rent for Month");//Amount for rent
-parseInt = amountRent
+parseInt = (amountRent)
 var amountExcel = prompt("Enter total for Excel monthly bill"); //Amount for Excel
-parseInt = amountExcel
+parseInt = (amountExcel)
 var amountCable = prompt("Enter total for Comcast monthly bill"); //Amount for Cable
-parseInt = amountCable
+parseInt = (amountCable)
 var amountGrocery = prompt("Enter estimated Grocery monthly bill"); // Amount for Groceries
-parseInt = amountGrocery
+parseInt = (amountGrocery)
 //var amountPaycheck = prompt("Enter paycheck amount")
 
 var totalBills = amountRent + amountExcel + amountCable + amountGrocery;

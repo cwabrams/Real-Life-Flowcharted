b7a38187e742b723cde62545f2188c88e070a00b
cwabrams <cwabrams@fullsail.edu>
2013-04-17 23:31:02 -0600

    Concatenating Practice

--- What's under the hood/js/script.js
+++ What's under the hood/js/script.js
@@ -1,6 +1,7 @@
 // JavaScript
 
-var orangeBins = [87, 234, 567, 883];
+var firstName = "Charlie";
+var lastName = "Abrams";
 
-var total= orangeBins[0] + orangeBins[1] + orangeBins[2] + orangeBins[3];
-console.log(total); 
+var fullName = firstName + " " + lastName;
+console.log(fullName); 

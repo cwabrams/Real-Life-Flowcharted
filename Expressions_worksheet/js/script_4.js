6f94a80d102b1af26aa11baab9bbb8367d871940
cwabrams <cwabrams@fullsail.edu>
2013-04-18 01:19:42 -0600

    Avg Grocery
    Addec comments, ensured code working properly

--- Expressions_worksheet/js/script_4.js
+++ Expressions_worksheet/js/script_4.js
@@ -1,5 +1,13 @@
 //Charles Abrams 4-17-2013 Expression Worksheet
-// JavaScript for Discounts
+// JavaScript for Average shopping bill
+
+var weeklyGrocery = [125, 145, 95, 110, 100] //Array of grocery bills
+
+totalSpent = weeklyGrocery [0] + weeklyGrocery [1] + weeklyGrocery [2] + weeklyGrocery [3] + weeklyGrocery [4] //Total for 5 weeks
+avgSpent = totalSpent/5 // Average Spent
+
+console.log(totalSpent) //Printing total spent
+console.log(avgSpent) //Printing average spent
 
 
 

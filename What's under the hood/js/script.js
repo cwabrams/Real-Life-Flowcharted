9782ff5eee5cc256ec9516d2cb4ed74b0d7ade10
cwabrams <cwabrams@fullsail.edu>
2013-04-17 23:04:15 -0600

    Pemdas Practice

--- What's under the hood/js/script.js
+++ What's under the hood/js/script.js
@@ -1,7 +1,9 @@
 // JavaScript
 
-var width = 3;
-var height = 6;
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
-var area= width * height/2;
-console.log(area); 
+var average= (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average); 

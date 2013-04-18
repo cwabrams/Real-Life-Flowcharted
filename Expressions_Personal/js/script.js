e4f807e5f0f16dbf2c9c62e65267e0b0ceaef9f7
cwabrams <cwabrams@fullsail.edu>
2013-04-18 03:28:49 -0600

    parseInt Test

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,10 +3,13 @@
 //Monthly Required Expense report for my family
 
 var amountRent = prompt("Enter total rent for Month");//Amount for rent
-parseInt amountRent
+parseInt = amountRent
 var amountExcel = prompt("Enter total for Excel monthly bill"); //Amount for Excel
+parseInt = amountExcel
 var amountCable = prompt("Enter total for Comcast monthly bill"); //Amount for Cable
+parseInt = amountCable
 var amountGrocery = prompt("Enter estimated Grocery monthly bill"); // Amount for Groceries
+parseInt = amountGrocery
 //var amountPaycheck = prompt("Enter paycheck amount")
 
 var totalBills = amountRent + amountExcel + amountCable + amountGrocery;

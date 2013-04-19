dfcbf46a277ecf9053cd3f890d053f93a35e802a
cwabrams <cwabrams@fullsail.edu>
2013-04-18 18:14:43 -0600

    Final comments

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,11 +1,11 @@
 // Charles Abrams 4-17-13 Expressions_Personal
 //Javascript
 //Monthly Required Expense report for my family:  This is meant to be used by anyone for basic bills, but has specific fields added for my girlfriend's use.
-//Default values removed for purposes of assignment
+//Default values removed for purposes of assignment, can be added back in at end of var statements
 
 var amountRent = parseInt(prompt("Enter total rent for Month"));//Amount for rent
-var amountExcel = parseInt(prompt("Enter total for Excel monthly bill")); //Amount for Excel
-var amountCable = parseInt(prompt("Enter total for Comcast monthly bill")); //Amount for Cable
+var amountEnergy = parseInt(prompt("Enter total for Energy bill")); //Amount for Energy
+var amountCable = parseInt(prompt("Enter total for Cable monthly bill")); //Amount for Cable
 var amountGrocery = parseInt(prompt("Enter estimated Grocery monthly bill")); // Amount for Groceries
 var miscExpense = parseInt(prompt("Enter Miscellaneous expense amount")); //Added misc. expense amount 
 var miscName = prompt("Enter reason for Miscellaneous expense"); //Bringing Misc reason as string
@@ -13,9 +13,9 @@ var amountPaycheck = parseInt(prompt("Enter paycheck amount")); //Amount for pay
 
 
 
-var totalBills = amountRent + amountExcel + amountCable + amountGrocery + miscExpense; //Bill total calculation
+var totalBills = amountRent + amountEnergy + amountCable + amountGrocery + miscExpense; //Bill total calculation
 var amountLeft = amountPaycheck - totalBills; //Money after bills
-var suggestedSavings = amountLeft * .4; //Suggested savings calculation
+var suggestedSavings = amountLeft * .4; //Suggested savings calculation 40%
 var spendingMoney = amountLeft - suggestedSavings //Leftover spending money calculation
 
 console.log("Our total bills were" + " $" + totalBills + " " + "and the potential savings for the month equals" + " $" + amountLeft);
@@ -24,6 +24,6 @@ console.log("Suggested amount to commit to savings account equals" + " $" + sugg
 console.log("Spending money available equals" + " $" + spendingMoney);//Printouts
 
 
-//console.log(totalBills)
-//console.log(amountLeft)
+//Everyday savings plan that my girlfriend and I have been working on
+
 

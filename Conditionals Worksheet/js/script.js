16a4375b33bc0465b0197be41c2bad1e2641e809
cwabrams <cwabrams@fullsail.edu>
2013-04-22 21:11:08 -0600

    Editing starter files

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -1,18 +1,3 @@
-// JavaScript practice week 3
+// Charles Abrams WPF Week 3 Conditionals Worksheet
 
-var kidHeight = 44;
-	minHeight = 48;
-	wParentHeight = 45;
 
-//if child is old enough, print to console "you can ride!"
-
-if(kidHeight > minHeight){
-	//you can ride
-	console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-	//you can ride with a parent
-	console.log("You can ride with a parent");
-}else{
-	//you cannot ride
-	console.log("Too bad, you cannot ride the coaster");
-}

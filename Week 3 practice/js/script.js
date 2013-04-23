09a1ccc5fde5d7e1919b8111b632ce8bbb6f458c
cwabrams <cwabrams@fullsail.edu>
2013-04-22 20:06:54 -0600

    Changes

--- Week 3 practice/js/script.js
+++ Week 3 practice/js/script.js
@@ -1,6 +1,6 @@
 // JavaScript practice week 3
 
-var kidHeight = 30
+var kidHeight = 50
 	minHeight = 48;
 
 //if child is old enough, print to console "you can ride!"
@@ -9,6 +9,6 @@ if(kidHeight > minHeight){
 	//code to be performed if the condition is true
 	console.log("You can ride the coaster!");
 }
-else(kidHeight < minHeight){
+if(kidHeight <= minHeight){
 	console.log("Too bad, you cannot ride the coaster");
 }

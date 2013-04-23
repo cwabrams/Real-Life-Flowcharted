8102fc73b6afe265a31166c70e1ca5bba7b075fe
cwabrams <cwabrams@fullsail.edu>
2013-04-22 20:38:11 -0600

    Testing logical operators

--- Logical operator practice/js/script.js
+++ Logical operator practice/js/script.js
@@ -1,4 +1,13 @@
 // Logical operator practice
 
+var budget = 300;
+	iPhonePrice = 199.99;
+	paycheck = 200; 
 
+//if the price of the phone is less than our budget
+if(iPhonePrice < budget){
+	console.log("We can buy the phone!")
+}else{
+	console.log("No phone for you!")
+}	
 

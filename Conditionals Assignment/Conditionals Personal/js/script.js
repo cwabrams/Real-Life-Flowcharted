3c0f8c54e1863cc61cea73001abe6111f1791278
cwabrams <cwabrams@fullsail.edu>
2013-04-24 10:02:41 -0600

    Saving changes
    Working through lines

--- Conditionals Assignment/Conditionals Personal/js/script.js
+++ Conditionals Assignment/Conditionals Personal/js/script.js
@@ -2,5 +2,14 @@
 //Goal of this code: To output if Rachele needs to eat/drink something to keep her blood sugar level up, or if she needs to 
 //give herself insulin
 
-
-
+var bloodSugar = parseInt(prompt("What if your current blood sugar level"); // User input on blod sugar
+	carbsEaten = parseInt(prompt("How many carbs did you recently consume?"); //User input on carbs eaten
+	insulinUnit = 1; 
+	carbsperUnit = 8;
+	 
+	
+if (carbsEaten = 0 && bloodSugar < 70){
+	console.log ("You need to eat something!")
+}else if (carbsEaten > 0){
+	console.log ("You need to enter" + carbsEaten / carbsperUnit)
+}

3be5268a4d7df1ce5410ece8b57f2a334fa4bb1c
cwabrams <cwabrams@fullsail.edu>
2013-04-24 15:44:11 -0600

    Adding printouts
    Wacky

--- Conditionals Assignment/Conditionals Wacky/js/script.js
+++ Conditionals Assignment/Conditionals Wacky/js/script.js
@@ -1,8 +1,17 @@
 // Charles Abrams Conditionals Assignment: Wacky 4-23
 // Is it beer Friday yet?
 
-var dayWeek = prompt("What day of the week is it?")
+var dayWeek = prompt("What day of the week is it? (case sensitive)")
 	lastFriday = prompt("Is it the last Friday of the month?")
 	working = prompt("Are you working on this day?")
+
+if (dayWeek = "Friday" && lastFriday = "Yes"){
+	console.log ("It's beer Friday!")
+}else if (dayWeek = "Monday"){
+	console.log ("I hate Mondays.")	
+}else{
+	console.log ("Another day, another dollar")
+}	
+
 	
-(lastFriday) ? console.log("It's beer Friday!") : console.log("Keep hoping!")
+//(lastFriday) ? console.log("It's beer Friday!") : console.log("Keep hoping!")

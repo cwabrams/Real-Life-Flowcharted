5366d50af84e266caf526fc93c6cf3072f95985c
cwabrams <cwabrams@fullsail.edu>
2013-04-24 09:29:24 -0600

    Group 3 commit

--- Conditionals Worksheet/Group 3/js/script.js
+++ Conditionals Worksheet/Group 3/js/script.js
@@ -1,3 +1,12 @@
 // Charles Abrams WPF Week 3 Conditionals Worksheet
 
-//Group 3
+//Group 3 
+
+//The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. 
+//In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+//Determine which of the two prices the customer is eligible for.
+
+var age = 40;
+var viewingTime = prompt("What time are you viewing the movie? HH:MM AM/PM format")
+var ticketPrice = 12;
+var discountPrice = 7;

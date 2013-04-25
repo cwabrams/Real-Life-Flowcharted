55868fac62457142bde5d6936f88f5ed0e8d0764
cwabrams <cwabrams@fullsail.edu>
2013-04-25 07:09:48 -0600

    Added more comments and final.

--- Conditionals Assignment/Conditionals Wacky/js/script.js
+++ Conditionals Assignment/Conditionals Wacky/js/script.js
@@ -2,18 +2,21 @@
 // Is it beer Friday yet?
 
 var dayWeek = prompt("What day of the week is it? (case sensitive)"); //What day of the week is it?
-	lastFriday = prompt("Is it the last Friday of the month?");
-	working = prompt("Are you working on this day?");
+	lastFriday = prompt("Is it the last Friday of the month?"); //Asking if it is the last Friday of the month
+	working = prompt("Are you working on this day?"); //Asking the user if they are working?
 
 if (dayWeek == "Friday" && lastFriday == "Yes"){
-	console.log ("It's beer Friday!");
+	console.log ("It's beer Friday!"); //Printout for beer Friday
 }else if (dayWeek == "Monday" && working == "Yes"){
-	console.log ("I hate Mondays.");
+	console.log ("I hate Mondays."); //Garfield printout of hating Mondays
 }else if (dayWeek == "Wednesday" && working == "Yes"){
-	console.log ("Hump day! Almost there!");
+	console.log ("Hump day! Almost there!"); //Hump day printout
 }else{
-	console.log ("Another day, another dollar");
+	console.log ("Another day, another dollar"); //All other entries will fall to this one
 }	
+
+
+//This was just a wacky one that I thought of.  At my work we have a beer Friday day each month to relax after month end. 
 
 	
 //(lastFriday) ? console.log("It's beer Friday!") : console.log("Keep hoping!")

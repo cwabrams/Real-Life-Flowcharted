0e747ec097f6d587ebb2e80ffa1cfc63073d8cd8
cwabrams <cwabrams@fullsail.edu>
2013-04-25 06:28:00 -0600

    Setting ternary

--- Conditionals Assignment/Conditionals Industry/js/script.js
+++ Conditionals Assignment/Conditionals Industry/js/script.js
@@ -1,6 +1,10 @@
 // Charles Abrams Conditionals Assignment: Industry 4-23
 // Creating code to tell me which ticket to work next at work based on priority and client
 
+var working = true // Let's us know if the user is working or not 
+
+(working) ? console.log("Start working tickets!") : console.log("Enjoy your day off!")
+
 var client = [TE, APC, JUN, POL] // List of 4 of our client ID's
 var priority = [0, 1, 2, 3] // Priority 0 is system down, 1 is high priority, 2 is normal, 3 is low
 

a2c51d485f4c7342c7c01032f61b168bb9669bd9
cwabrams <cwabrams@fullsail.edu>
2013-04-25 06:48:51 -0600

    Added ticket 2 section

--- Conditionals Assignment/Conditionals Industry/js/script.js
+++ Conditionals Assignment/Conditionals Industry/js/script.js
@@ -12,17 +12,31 @@ var ticketOnepriority = prompt("Enter priority of ticket."); //Enter priority
 
 workingTicket = (ticketOne) ? console.log("Work the TE Ticket") : console.log("Move onto the next ticket"); //TE first status check
 if (ticketOne == true && ticketOnepriority == "0"){
-	console.log "Turnaround time is 2 hours for this ticket"
+	console.log ("Turnaround time is 2 hours for this ticket")
 }else if (ticketOne == true && ticketOnepriority == "1"){
-	console.log "Turnaround time is 8 hours for this ticket"
+	console.log ("Turnaround time is 8 hours for this ticket")
 }else if (ticketOne == true && ticketOnepriority == "2"){
-	console.log "Turnaround time is 24 hours on this ticket"	
+	console.log ("Turnaround time is 24 hours on this ticket")	
 }else{
-	console.log "Turnaround time is 72 hours on this ticket"
+	console.log ("Turnaround time is 72 hours on this ticket")
 }	
 
 
-//var
+var ticketTwo = confirm("Do you have any APC tickets?"); //APC is second highest priority
+var ticketTwopriority = prompt("Enter priority of ticket."); //Enter priority
+
+workingTickettwo = (ticketTwo) ? console.log("Work the APC Ticket") : console.log("Move onto the next ticket"); //APC second status check
+if (ticketTwo == true && ticketTwopriority == "0"){
+	console.log ("Turnaround time is 2 hours for this ticket")
+}else if (ticketTwo == true && ticketTwopriority == "1"){
+	console.log ("Turnaround time is 8 hours for this ticket")
+}else if (ticketTwo == true && ticketTwopriority == "2"){
+	console.log ("Turnaround time is 24 hours on this ticket")	
+}else{
+	console.log ("Turnaround time is 72 hours on this ticket")
+}	
+
+
 
 
 

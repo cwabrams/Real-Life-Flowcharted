bed47a06258aa5d87fc7f6a411c4c38a44d89296
cwabrams <cwabrams@fullsail.edu>
2013-04-24 23:23:17 -0600

    Start of industry code

--- Conditionals Assignment/Conditionals Industry/js/script.js
+++ Conditionals Assignment/Conditionals Industry/js/script.js
@@ -1,2 +1,7 @@
 // Charles Abrams Conditionals Assignment: Industry 4-23
 // Creating code to tell me which ticket to work next at work based on priority and client
+
+var client = [TE, APC, JUN, POL] // List of 4 of our client ID's
+var priority = [0, 1, 2, 3] // Priority 0 is system down, 1 is high priority, 2 is normal, 3 is low
+
+

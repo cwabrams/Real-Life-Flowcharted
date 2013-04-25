f8649eeee946934d2f4c78b071769e127eb9a6e0
cwabrams <cwabrams@fullsail.edu>
2013-04-25 06:43:05 -0600

    Setting ifelse for TE

--- Conditionals Assignment/Conditionals Industry/js/script.js
+++ Conditionals Assignment/Conditionals Industry/js/script.js
@@ -8,9 +8,14 @@ status = (working) ? console.log("Start working tickets!") : console.log("Enjoy 
 var client = ["TE", "APC", "JUN", "POL"]; // List of 4 of our client ID's (priority as listed)
 var priority = [0, 1, 2, 3]; // Priority 0 is system down, 1 is high priority, 2 is normal, 3 is low
 var ticketOne = confirm("Do you have any TE tickets?"); //TE is highest priority
-var ticketTwo = prompt("Enter priority of ticket."); //Enter priority
+var ticketOnepriority = prompt("Enter priority of ticket."); //Enter priority
 
 workingTicket = (ticketOne) ? console.log("Work the TE Ticket") : console.log("Move onto the next ticket"); //TE first status check
+if (ticketOne == true && ticketOnepriority == "0"){
+	console.log "Turnaround time is 2 hours for this ticket"
+}else if (ticketOne ==
+
+var
 
 
 

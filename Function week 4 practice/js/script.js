d03ab2b123fad0f5b586ff11ab76008014f0640b
cwabrams <cwabrams@fullsail.edu>
2013-04-28 11:05:36 -0600

    Commented out/made new changes for testing

--- Function week 4 practice/js/script.js
+++ Function week 4 practice/js/script.js
@@ -7,20 +7,22 @@
 //Can invoke the code more than once, essentially making it reusable. calcArea(); is the command
 //Materials in our code = arguments.  Bins to store the materials or code are called parameters
 
-function outputMsg (){
-	console.log("Hello world");
-}
+//function outputMsg (){
+	//console.log("Hello world");
+//}
 
-var width = 5; //This variable is scoped outside of the function
+//var width = 5; //This variable is scoped outside of the function
 
-function calcArea (){
-	var width = 20; //This variable is scoped INSIDE the function
-		height = 30;
-		area = width * height;
-		console.log(area);
-}	
-console.log(width); 
-calcArea(); 
+//function calcArea (){
+	//var width = 20; //This variable is scoped INSIDE the function
+		//height = 30;
+		//area = width * height;
+		//console.log(area);
+//}	
+//console.log(width); 
+//calcArea(); 
+
+//Commented out for notes
 
 
 //Basic function layout
@@ -28,3 +30,10 @@ calcArea();
 //function funcName(parameter1, parameter2){
 	//code the function runs
 //}	
+
+calcArea(30, 20); //Arguments that will be used in the function
+
+function CalcArea(w, h){ //Bins that are storing the arguments
+	var area = w * h;
+	console.log(area);
+}	

908b31cdc6df40c24d36ba6739d427b73d077c95
cwabrams <cwabrams@fullsail.edu>
2013-04-30 23:22:13 -0600

    Starting final else statement

--- Functions Wacky/js/script.js
+++ Functions Wacky/js/script.js
@@ -8,10 +8,12 @@ var conversionCalc = function(){
 	proceed = (liquid) ? console.log("Great, you are working with a liquid") : console.log("You are not using a liquid"); //Printout
 	var conversionType = confirm("Select OK for gallons and cancel for liter conversions"); 
 if (conversionType == true){
-	var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups."))
-		conversionGalcup = 0.0625 // Gallons per one cup
-		calcGalcup = liquidCups * conversionGalcups
-
+	var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups."));
+		conversionGalcup = 0.0625; // Gallons per one cup
+		calcGalcup = liquidCups * conversionGalcups;
+		return calcGalcup;
+}else
+	var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups."));
 
 
 

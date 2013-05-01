f8736e9188882718db2fd4a8bf42cc24cd150670
cwabrams <cwabrams@fullsail.edu>
2013-04-30 20:49:59 -0600

    Another test

--- Functions worksheet/Circumference practice/js/script.js
+++ Functions worksheet/Circumference practice/js/script.js
@@ -1,11 +1,12 @@
 // Circumference practice(functions)
 //Calculating the circumference of a circle 
 
-var circumference = function((radius * 2) * 3.14){ //setting the function equal to the variable 
-	var radius = parseInt(prompt("Please enter the radius of the circle"))
+var circumferenceCalc = function(radius, d, pi){ //setting the function equal to the variable 
+	var radius = parseInt(prompt("Please enter the radius of the circle"));
+	var circumference = (radius * d) * pi;
 	return circumference
 }
 
-var circum = circumference;
+var cir = circumferenceCalc(3,2,3.14);
 
 //var radius = parseInt(prompt("Please enter the radius of your circle"))

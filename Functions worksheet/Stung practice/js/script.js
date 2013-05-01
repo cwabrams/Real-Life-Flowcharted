bbe4af99adf5283865f58ff78d06f28872cda3d4
cwabrams <cwabrams@fullsail.edu>
2013-04-30 21:22:42 -0600

    Added syntax for printout

--- Functions worksheet/Stung practice/js/script.js
+++ Functions worksheet/Stung practice/js/script.js
@@ -4,7 +4,7 @@ function stingVictim(){
 	var weight = parseInt(prompt("Please enter the animal's weight in pounds")); //User entry for weight
 		beestings = 8.66666667; //Constant: Number of beestings that it takes to kill an animal per pound
 		stingsPerpound = weight * beestings;
-		console.log(stingsPerpound);
+		console.log("It will take " + stingsPerpound + " to kill an animal that size.");
 }
 
 stingVictim();

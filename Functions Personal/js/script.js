41b731d2980bc4fdf11a8b0efe58eee668fb66c1
cwabrams <cwabrams@fullsail.edu>
2013-04-30 21:53:32 -0600

    Adding confirmation that it is a work week

--- Functions Personal/js/script.js
+++ Functions Personal/js/script.js
@@ -4,10 +4,12 @@
 //Use Work time, school time, cleaning time, raid time and avg sleep per night
 
 function freeTime(){
-	var workTime = parseInt(prompt("Enter how many hours on average you work per week"))
-		schoolTime = parseInt(prompt("Enter how many hours you need to spend studying per week"))
-		cleaningTime = parseInt(prompt("Enter how many hours you spend on chores per week"))
-		raidTime = parseInt(prompt("Enter how many hours of gaming time you spend per week"))
-		sleepTime = parseInt(prompt("
-
+	var 
+		workTime = parseInt(prompt("Enter how many hours on average you work per week"));
+		schoolTime = parseInt(prompt("Enter how many hours you need to spend studying per week"));
+		cleaningTime = parseInt(prompt("Enter how many hours you spend on chores per week"));
+		raidTime = parseInt(prompt("Enter how many hours of gaming time you spend per week"));
+		sleepTime = parseInt(prompt("Enter how many hours you need to sleep per night"));
+		totalTime = 168;
+		
 }

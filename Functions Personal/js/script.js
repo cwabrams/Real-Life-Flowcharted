1bcb36cd1cb13bbd2e4e71145d8bb20b83ecad9f
cwabrams <cwabrams@fullsail.edu>
2013-04-30 22:13:58 -0600

    Starting to add comments

--- Functions Personal/js/script.js
+++ Functions Personal/js/script.js
@@ -3,10 +3,10 @@
 //Based on how many hours in a week, calculate how much free time I will have
 //Use Work time, school time, cleaning time, raid time and avg sleep per night
 
-function freeTimecalc(){
-	var working = confirm("Is this a work week?");
-if (working == true){	
-	var	workTime = parseInt(prompt("Enter how many hours on average you work per week"));
+function freeTimecalc(){ //Setting the function up
+	var working = confirm("Is this a work week?"); //Prompt from user to find out if you work this week or not
+if (working == true){ //Start of elseif statement	
+	var	workTime = parseInt(prompt("Enter how many hours on average you work per week")); //Prompt for work hours
 		schoolTime = parseInt(prompt("Enter how many hours you need to spend studying per week"));
 		cleaningTime = parseInt(prompt("Enter how many hours you spend on chores per week"));
 		raidTime = parseInt(prompt("Enter how many hours of gaming time you spend per week"));

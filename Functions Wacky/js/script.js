28acf51227061ac3b93e9f33a89ce023aa76642e
cwabrams <cwabrams@fullsail.edu>
2013-04-30 23:18:43 -0600

    Start of first ifelse statement

--- Functions Wacky/js/script.js
+++ Functions Wacky/js/script.js
@@ -5,4 +5,31 @@
 var conversionCalc = function(){
 
 	var liquid = true;
-	proceed = (liquid) ? console.log("Great, you are working with a liquid") : console.log("You are not using a liquid"); //Printout for working status
+	proceed = (liquid) ? console.log("Great, you are working with a liquid") : console.log("You are not using a liquid"); //Printout
+	var conversionType = confirm("Select OK for gallons and cancel for liter conversions"); 
+if (conversionType == true){
+	var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups."))
+		conversionGalcup = 0.0625 // Gallons per one cup
+		calcGalcup = liquidCups * conversionGalcups
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

e01bc6551505d9200ac11876073cddc2eb1c9e60
cwabrams <cwabrams@fullsail.edu>
2013-04-30 21:30:14 -0600

    Created directories for new assignments

---
+++ Functions Industry/js/script.js
@@ -0,0 +1,10 @@
+// Sting practice (functions)
+
+function stingVictim(){
+	var weight = parseInt(prompt("Please enter the animal's weight in pounds")); //User entry for weight
+		beestings = 8.66666667; //Constant: Number of beestings that it takes to kill an animal per pound
+		stingsPerpound = weight * beestings; //Calculation
+		console.log("It will take " + stingsPerpound + " to kill an animal that size."); //Printout
+}
+
+stingVictim(); //Calling the function
---
+++ Functions Personal/js/script.js
@@ -0,0 +1,3 @@
+// Assignment: Functions Personal
+
+//Explaining what the code will do

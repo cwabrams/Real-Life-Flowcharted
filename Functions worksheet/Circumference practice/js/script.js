ea52cfa37e9d0a1409699146a5a2557fecbc3824
cwabrams <cwabrams@fullsail.edu>
2013-04-30 20:55:59 -0600

    Set consolelog

--- Functions worksheet/Circumference practice/js/script.js
+++ Functions worksheet/Circumference practice/js/script.js
@@ -2,11 +2,14 @@
 //Calculating the circumference of a circle 
 
 var circumferenceCalc = function(radius, d, pi){ //setting the function equal to the variable 
-	var radius = parseInt(prompt("Please enter the radius of the circle"));
+	var radius = parseInt(prompt("Please enter the radius of the circle in inches"));
+	var d = 2;
+	var pi = 3.14
 	var circumference = (radius * d) * pi;
 	return circumference
 }
 
-var cir = circumferenceCalc(3,2,3.14);
+var cir = circumferenceCalc();
+console.log("Your circle has a circumference of " + cir + " inches.");
 
 //var radius = parseInt(prompt("Please enter the radius of your circle"))

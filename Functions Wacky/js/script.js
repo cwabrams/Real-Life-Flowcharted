c46fe78b29b510c9e817a158498edceacce85232
cwabrams <cwabrams@fullsail.edu>
2013-04-30 23:09:15 -0600

    Setting up basis for function
    With Ternary

--- Functions Wacky/js/script.js
+++ Functions Wacky/js/script.js
@@ -1,2 +1,8 @@
 // Charles Abrams 04-30 Assignment: Functions Wacky
 // Using this to calcule gallons to cups or liters based on user input. (Use ternary)
+
+
+var conversionCalc = function(){
+
+	var liquid = true;
+	proceed = (liquid) ? console.log("Great, you are working with a liquid") : console.log("You are not using a liquid"); //Printout for working status

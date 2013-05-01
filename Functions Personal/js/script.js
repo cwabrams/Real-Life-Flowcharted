047a05dccb1548db6f1129bc45e956d424eb7d7f
cwabrams <cwabrams@fullsail.edu>
2013-04-30 21:45:54 -0600

    Adding function

--- Functions Personal/js/script.js
+++ Functions Personal/js/script.js
@@ -1,3 +1,13 @@
 // Assignment: Functions Personal
 
-//Explaining what the code will do
+//Based on how many hours in a week, calculate how much free time I will have
+//Use Work time, school time, cleaning time, raid time and avg sleep per night
+
+function freeTime(){
+	var workTime = parseInt(prompt("Enter how many hours on average you work per week"))
+		schoolTime = parseInt(prompt("Enter how many hours you need to spend studying per week"))
+		cleaningTime = parseInt(prompt("Enter how many hours you spend on chores per week"))
+		raidTime = parseInt(prompt("Enter how many hours of gaming time you spend per week"))
+		sleepTime = parseInt(prompt("
+
+}

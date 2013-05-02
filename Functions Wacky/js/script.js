59bb38f6a9d04b87490b21c435021b31581d823c
cwabrams <cwabrams@fullsail.edu>
2013-05-01 22:22:12 -0600

    Edits that I did at work and tested

--- Functions Wacky/js/script.js
+++ Functions Wacky/js/script.js
@@ -1,20 +1,30 @@
 // Charles Abrams 04-30 Assignment: Functions Wacky
 // Using this to calcule gallons to cups or liters based on user input. (Use ternary)
 
-
 var conversionCalc = function(){
+    var liquid = confirm("Select OK if you are working with a liquid");
+    proceed = (liquid) ? console.log("Great, you are working with a liquid") : console.log("You are not using a liquid"); //Printout
+    var conversionType = confirm("Select OK for gallons and cancel for liter conversions"); 
+if (liquid == true && conversionType == true){
+    var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups.")); //Entered from user
+        conversionGalcups = 0.0625; // Gallons per one cup
+        calcGalcup = liquidCups * conversionGalcups; //Calculation for cups to gallons
+return ("You have " + calcGalcup + " gallon(s) of liquid."); 
+}else if(liquid == true && conversionType == false){
+    var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups.")); //Entered from user
+   conversionLitercups = 0.236588; //How many liters are in 1 cup
+   calcLitercup = liquidCups * conversionLitercups; //Calculation for cups to liters 
+return ("You have " + calcLitercup + " liter(s) of liquid."); 
+}else if(liquid == false){
+console.log("Please try your entry again.");
+}else 
+console.log("Please try your entry again.");
+}	
+var conversion = conversionCalc();
+console.log(conversion); 
 
-	var liquid = true;
-	proceed = (liquid) ? console.log("Great, you are working with a liquid") : console.log("You are not using a liquid"); //Printout
-	var conversionType = confirm("Select OK for gallons and cancel for liter conversions"); 
-if (conversionType == true){
-	var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups."));
-		conversionGalcup = 0.0625; // Gallons per one cup
-		calcGalcup = liquidCups * conversionGalcups;
-		return calcGalcup;
-}else
-	var liquidCups = parseInt(prompt("Please enter the amount of liquid in cups."));
 
+//Still having issues with the first confirm not just kicking to an error. 
 
 
 

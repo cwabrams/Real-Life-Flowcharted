eaa4e6b4380b0c544f1b1915648b22eda424a1ab
cwabrams <cwabrams@fullsail.edu>
2013-05-01 23:31:34 -0600

    Stopping point for the evening.

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -7,10 +7,11 @@ function calculateTime(w,b,j){ //Using parameters here
 	var route = parseInt(prompt("Please enter which route number you are taking?"));
 	console.log ("You have chosen route " + route + ".")
 	var totalTime = w + b + j;
-	console.log (totalTime);
+	console.log ("It will take you " + totalTime + " minutes to get to work.");
 }
+
 calculateTime(2,15,4); //Enter values for w, b and j here
-console.log("It will take you " + totalTime + " minutes to get to work.");
+//console.log ("It will take you " + totalTime + " minutes to get to work.");
 
 	/*  Values to use for the two routes I take. 
 		b	route1 = 15; 
@@ -18,3 +19,5 @@ console.log("It will take you " + totalTime + " minutes to get to work.");
 		w	walkHome = 2; 
 		j	walkWorkone = 4; 
 		j	walkWorksix = 6; */
+		
+// Need to add final notes and attempt to get the values to be entered by a prompt.  

88319659e8348bf44c2d8e17d7449d9b4e347523
cwabrams <cwabrams@fullsail.edu>
2013-05-01 22:29:00 -0600

    Industry started and files setup

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -1,10 +1,2 @@
-// Sting practice (functions)
+// Charles Abrams 4-30: Assignment Functions Industry
 
-function stingVictim(){
-	var weight = parseInt(prompt("Please enter the animal's weight in pounds")); //User entry for weight
-		beestings = 8.66666667; //Constant: Number of beestings that it takes to kill an animal per pound
-		stingsPerpound = weight * beestings; //Calculation
-		console.log("It will take " + stingsPerpound + " to kill an animal that size."); //Printout
-}
-
-stingVictim(); //Calling the function

c03ac5f8c14a771d23cc0dde87462d113077d4ee
cwabrams <cwabrams@fullsail.edu>
2013-05-01 23:24:30 -0600

    Gave up on elseif

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -3,25 +3,17 @@
 // Walk time from home to starting stop and final stop to final destination
 // 3 parameters in this one 
 
-function calculateTime(w, b, j){ //Using parameters here	
-	//proceed = (route) ? console.log("You selected Route 1") : console.log("You selected route 16"); //Printout
-	var route = parseInt(Prompt("Please enter which route number you are taking?"));
+function calculateTime(w,b,j){ //Using parameters here	
+	var route = parseInt(prompt("Please enter which route number you are taking?"));
 	var totalTime = w + b + j;
-if (route == 1){
-	calculateTime(2, 15, 4);
-}else if(route == 16){
-	calculateTime(2, 20, 6);
-}else{
-	console.log("Please try your entries again")
 }
-console.log(totalTime);
-}
+console.log("It will take you " + totalTime + " minutes to get to work.");
 
-calculateTime(); 
+calculateTime(2,15,4); //Enter values for w, b and j here
 
 
-	/*var route1 = 15; 
-		route16 = 20; 
-		walkHome = 2; 
-		walkWorkone = 4; 
-		walkWorksix = 6; */
+	b	route1 = 15; 
+	b	route16 = 20; 
+	w	walkHome = 2; 
+	j	walkWorkone = 4; 
+	j	walkWorksix = 6; */

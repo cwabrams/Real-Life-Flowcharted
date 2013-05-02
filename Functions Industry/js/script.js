87d9c1a1f66df76fba2658d700471dfdf925e935
cwabrams <cwabrams@fullsail.edu>
2013-05-02 07:24:36 -0600

    Trying new way of prompting user for arguments

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -3,14 +3,29 @@
 // Walk time from home to starting stop and final stop to final destination
 // 3 parameters in this one 
 
-function calculateTime(w,b,j){ //Using parameters here	
-	var route = parseInt(prompt("Please enter which route number you are taking?"));
+function calculateTime(walkingTime,busTime,journeyWalk){ //Using parameters here	
+	var route = parseInt(prompt("Please enter which route number you are taking (1 or 16)?"));
 	console.log ("You have chosen route " + route + ".")
-	var totalTime = w + b + j;
+	//var totalTime = w + b + j;
+if (route == 1){
+	var walkingTime = parseInt(prompt("Please enter the amount of time it takes to walk from your home to the bus stop (in minutes.)"));
+	var busTime = parseInt(prompt("Please enter the amount of time it is scheduled to take you on the bus (in minutes)."));
+	var journeyWalk = parseInt(prompt("Please enter the amount of time it takes to walk from your final stop to work (in minutes)."));
+	var totalTime = walkingTime + busTime + journeyWalk;
 	console.log ("It will take you " + totalTime + " minutes to get to work.");
-}
+}else if (route == 16)
+	var walkingTime = parseInt(prompt("Please enter the amount of time it takes to walk from your home to the bus stop (in minutes.)"));
+	var busTime = parseInt(prompt("Please enter the amount of time it is scheduled to take you on the bus (in minutes).");
+	var journeyWalk = parseInt(prompt("Please enter the amount of time it takes to walk from your final stop to work (in minutes)."));
+	var totalTime = walkingTime + busTime + journeyWalk;
+	console.log ("It will take you " + totalTime + " minutes to get to work.");
+	console.log ("It will take you " + totalTime + " minutes to get to work.");
+}else
+	console.log ("Please try your entry again.")
 
-calculateTime(2,15,4); //Enter values for w, b and j here
+calculateTime(); 
+
+
 //console.log ("It will take you " + totalTime + " minutes to get to work.");
 
 	/*  Values to use for the two routes I take. 

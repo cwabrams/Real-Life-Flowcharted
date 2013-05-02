464e068ec0bcd49cba5342235a749618971d4640
cwabrams <cwabrams@fullsail.edu>
2013-05-01 22:39:04 -0600

    Setting up basic parameters

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -3,4 +3,8 @@
 // Walk time from home to starting stop and final stop to final destination
 // 3 parameters in this one 
 
-var routeChoice = function(){
+function calculateTime(w, b, j){ //Using parameters here
+	var totalTime = w + b + j;
+	console.log(area);
+}
+calculateArea(20, 30); //Arguments entered here

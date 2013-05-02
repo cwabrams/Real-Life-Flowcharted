1c69f7519c814956470fca2bf4dd267b0f64c70f
cwabrams <cwabrams@fullsail.edu>
2013-05-01 22:31:00 -0600

    More notes

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -1,2 +1,6 @@
-// Charles Abrams 4-30: Assignment Functions Industry
+// Charles Abrams 04-30 Assignment: Functions Industry
+// commuting time based on route selection, amount of stops and speed: two choices, 16 and 1 routes. 
+// Walk time from home to starting stop and final stop to final destination
+// 3 parameters in this one 
 
+var routeChoice = function(){

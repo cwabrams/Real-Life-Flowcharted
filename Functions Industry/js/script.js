e058a71ff6d8a7feac43bb5f5c8c6e07ee0b3876
cwabrams <cwabrams@fullsail.edu>
2013-05-02 07:29:49 -0600

    WORKING
    Need to add final notes (prompt from user for parameters is now
    working.

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -7,13 +7,13 @@ function calculateTime(walkingTime,busTime,journeyWalk){ //Using parameters here
 	var route = parseInt(prompt("Please enter which route number you are taking (1 or 16)?"));
 	console.log ("You have chosen route " + route + ".")
 	//var totalTime = w + b + j;
-	if (route == 1){
+if (route == 1){
 		var walkingTime = parseInt(prompt("Please enter the amount of time it takes to walk from your home to the bus stop (in minutes.)"));
 		var busTime = parseInt(prompt("Please enter the amount of time it is scheduled to take you on the bus (in minutes)."));
 		var journeyWalk = parseInt(prompt("Please enter the amount of time it takes to walk from your final stop to work (in minutes)."));
 		var totalTime = walkingTime + busTime + journeyWalk;
 		console.log ("It will take you " + totalTime + " minutes to get to work.");
-	}else if{ (route == 16)
+}else if (route == 16){
 		var walkingTime = parseInt(prompt("Please enter the amount of time it takes to walk from your home to the bus stop (in minutes.)"));
 		var busTime = parseInt(prompt("Please enter the amount of time it is scheduled to take you on the bus (in minutes)."));
 		var journeyWalk = parseInt(prompt("Please enter the amount of time it takes to walk from your final stop to work (in minutes)."));
@@ -25,14 +25,5 @@ function calculateTime(walkingTime,busTime,journeyWalk){ //Using parameters here
 }
 calculateTime(); 
 
-
-//console.log ("It will take you " + totalTime + " minutes to get to work.");
-
-	/*  Values to use for the two routes I take. 
-		b	route1 = 15; 
-		b	route16 = 20; 
-		w	walkHome = 2; 
-		j	walkWorkone = 4; 
-		j	walkWorksix = 6; */
 		
-// Need to add final notes and attempt to get the values to be entered by a prompt.  
+// Need to add final notes.

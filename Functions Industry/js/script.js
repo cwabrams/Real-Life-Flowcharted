1e3c7c54c7bfcf4b6eb3da78828e8b6553e1b1d1
cwabrams <cwabrams@fullsail.edu>
2013-05-01 22:51:14 -0600

    Removed extra lines

--- Functions Industry/js/script.js
+++ Functions Industry/js/script.js
@@ -3,9 +3,6 @@
 // Walk time from home to starting stop and final stop to final destination
 // 3 parameters in this one 
 
-var route = confirm("Select OK if you are taking route 1 or select cancel if you are taking route 16.")	
-proceed = (route) ? console.log("Great, you are working with a liquid") : console.log("You are not using a liquid"); //Printout
-
 function calculateTime(w, b, j){ //Using parameters here
 	var route = confirm("Select OK if you are taking route 1 or select cancel if you are taking route 16.")	
 proceed = (route) ? console.log("You selected Route 1") : console.log("You selected route 16"); //Printout
@@ -15,6 +12,6 @@ proceed = (route) ? console.log("You selected Route 1") : console.log("You selec
 		walkWorkone = 4; 
 		walkWorksix = 6; 
 	var totalTime = w + b + j;
-	console.log(area);
+	console.log(totalTime);
 }
 calculateTime(2, 15, 4); //Arguments entered here
